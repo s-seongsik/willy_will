8bc3cf1b06177ab6f937b283adae907d73dfb269
Shin <smy11go@gmail.com>
2020-02-08 23:06:15 +0900

    [2020-02-08] 23:05

---
+++ app/src/main/java/com/example/willy_will/GroupListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.willy_will;
+
+public class GroupListAdapter {
+}
---
+++ app/src/main/java/com/example/willy_will/activitySearchSetting.java
@@ -0,0 +1,14 @@
+package com.example.willy_will;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityGroupSearchSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_search_setting);
+    }
+}
