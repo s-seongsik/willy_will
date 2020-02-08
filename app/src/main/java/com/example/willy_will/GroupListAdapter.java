bff576c6db207b85d4e4072f30a7713b76c1e080
Shin <smy11go@gmail.com>
2020-02-08 23:22:26 +0900

    [2020-02-08 23:21] Search View & Popup

--- app/src/main/java/com/example/willy_will/GroupListAdapter.java
+++ app/src/main/java/com/example/willy_will/GroupListAdapter.java
@@ -1,4 +1,100 @@
 package com.example.willy_will;
 
-public class GroupListAdapter {
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GroupListAdapter extends RecyclerView.Adapter<GroupListAdapter.GroupListViewHolder> {
+
+    /**
+     * https://developer.android.com/guide/topics/ui/layout/recyclerview#java
+     */
+
+    /**
+     * https://recipes4dev.tistory.com/154
+     * https://recipes4dev.tistory.com/167
+     */
+
+    private String[] dataset;
+
+    /**
+     * Last Modified: -
+     * Last Modified By: -
+     * Created: 2020-02-08
+     * Created By: Shin Minyong
+     * Function: Provide a reference to the views for each data item
+     * Complex data items may need more than one view per item, and
+     * you provide access to all the views for a data item in a view holder.
+     */
+    public static class GroupListViewHolder extends RecyclerView.ViewHolder{
+        public TextView textView;
+        public GroupListViewHolder(TextView txtV) {
+            super(txtV);
+            textView = txtV;
+        }
+    }
+
+    /**
+     * Last Modified: -
+     * Last Modified By: -
+     * Created: 2020-02-08
+     * Created By: Shin Minyong
+     * Function: Provide a suitable constructor (depends on the kind of dataset)
+     * @param groupDataset
+     */
+    public GroupListAdapter(String[] groupDataset) {
+        dataset = groupDataset;
+    }
+
+    /**
+     * Last Modified: 2020-02-08
+     * Last Modified By: Shin Minyong
+     * Created: -
+     * Created By: -
+     * Function: Create new views (invoked by the layout manager)
+     * @param parent
+     * @param viewType
+     * @return groupListViewHolder
+     */
+    @NonNull
+    @Override
+    public GroupListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView textView = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.group_text_view, parent, false);
+        // ...
+        GroupListViewHolder groupListViewHolder = new GroupListViewHolder(textView);
+        return groupListViewHolder;
+    }
+
+    /**
+     * Last Modified: 2020-02-08
+     * Last Modified By: Shin Minyong
+     * Created: -
+     * Created By: -
+     * Function: Replace the contents of a view (invoked by the layout manager)
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull GroupListViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(dataset[position]);
+    }
+
+    /**
+     * Last Modified: 2020-02-08
+     * Last Modified By: Shin Minyong
+     * Created: -
+     * Created By: -
+     * Function: Return the size of the dataset (invoked by the layout manager)
+     * @return dataset.length
+     */
+    @Override
+    public int getItemCount() {
+        return dataset.length;
+    }
 }
--- app/src/main/java/com/example/willy_will/activitySearchSetting.java
+++ app/src/main/java/com/example/willy_will/activitySearchSetting.java
@@ -1,14 +1,49 @@
 package com.example.willy_will;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class activityGroupSearchSetting extends AppCompatActivity {
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+public class activitySearchSetting extends Activity {
+
+    private RecyclerView groupListRecyclerView;
+    private RecyclerView.Adapter groupListAdapter;
+    private RecyclerView.LayoutManager groupListLayoutManager;
+
+    /**
+     * Last Modified: 2020-02-08
+     * Last Modified By: Shin Minyong
+     * Created: -
+     * Created By: -
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_search_setting);
+        // Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // ~Remove title bar
+        setContentView(R.layout.activity_search_setting);
+
+        // Set RecyclerView
+        groupListRecyclerView = (RecyclerView) findViewById(R.id.group_list_view);
+        groupListRecyclerView.setHasFixedSize(true);
+        groupListLayoutManager = new LinearLayoutManager(this);
+        groupListRecyclerView.setLayoutManager(groupListLayoutManager);
+        String[] groupListDataset = {"첫 번째 그룹", "두 번째 그룹"};
+        groupListAdapter = new GroupListAdapter(groupListDataset);
+        groupListRecyclerView.setAdapter(groupListAdapter);
     }
+
+    /**
+     * https://ghj1001020.tistory.com/9
+     */
+
+    /**
+     * https://developer.android.com/guide/topics/ui/layout/recyclerview#java
+     */
+
 }
