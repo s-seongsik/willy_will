5d554ae1c2acf9e5a65504ad606b1b0dbdeee2d1
helloWorld <woon_12@naver.com>
2020-02-22 14:35:49 +0900

    [데이타베이스]  - 테스트 중

--- app/src/main/java/com/willy/will/calander/view/fragmentCalander.java
+++ app/src/main/java/com/willy/will/calander/view/fragmentCalander.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.willy.will.R;
+import com.willy.will.dataBase.DBAccess;
 
 public class fragmentCalander extends Fragment {
     // example extra string
@@ -34,6 +36,9 @@ public class fragmentCalander extends Fragment {
     // Inflate the view for the fragment based on layout XML
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        DBAccess dbHelper = new DBAccess(getContext(), "test.db", null, 1);
+        dbHelper.insert("test","test", 1000);
+        Toast.makeText(this.getContext(), dbHelper.getDatabaseName(), Toast.LENGTH_LONG).show();
         View view = inflater.inflate(R.layout.fragment_calander, container, false);
         return view;
     }
